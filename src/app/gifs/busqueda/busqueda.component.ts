3411f6f888dee32ef6e3d9ab98c3035a74bcd725
titicuevas <enrique.cuevas@iesdonana.org>
2021-11-09 13:49:02 +0100

    Mostrar imagenes

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild} from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
@@ -6,11 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BusquedaComponent implements OnInit {
+export class BusquedaComponent  {
 
-  constructor() { }
+  @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
 
-  ngOnInit(): void {
+  constructor(private gifsService: GifsService )
+  {
+
+
+
+
   }
+
+
+ buscar(termino:string){
+
+  const valor = this.txtBuscar.nativeElement.value;
+
+  if (valor.trim().length===0) {
+    return;
+    
+  }
+  
+
+   this.gifsService.buscarGifs(valor);
+   
+   this.txtBuscar.nativeElement.value = '';
+ }
 
 }
---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GifsService {
+  private apiKey: string = 'kfmFFi4kLSf493tt2v2JUp9MfXTf5aB0';
+
+  private _historial: string[] = [];
+
+  public resultados: any[]=[];
+
+  get historial() {
+    return [...this._historial];
+  }
+
+  constructor(private http: HttpClient) {}
+
+  buscarGifs(query: string = '') {
+    query = query.trim().toLowerCase();
+
+    if (!this._historial.includes(query)) {
+      this._historial.unshift(query);
+      this._historial = this._historial.splice(0, 10);
+    }
+    this.http
+      .get(
+      `https://api.giphy.com/v1/gifs/search?api_key=kfmFFi4kLSf493tt2v2JUp9MfXTf5aB0&q=${ query }&limit=10`)
+      .subscribe((resp:any) => {
+        console.log(resp.data);
+        this.resultados = resp.data;
+      });
+  }
+}
